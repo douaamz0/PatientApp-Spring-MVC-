0fdd0328a0b870990c71da821e7f3ba8ce83d999
douaamz0 <douaa.code0@gmail.com>
2024-04-01 22:24:04 +0000

    first commit

---
+++ src/main/java/ma/emsi/patientapp/PatientAppApplication.java
@@ -0,0 +1,29 @@
+package ma.emsi.patientapp;
+
+import ma.emsi.patientapp.entities.Patient;
+import ma.emsi.patientapp.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientAppApplication implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientAppApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null,"Douaa",new Date(),false,12));
+        patientRepository.save(new Patient(null,"Amina",new Date(),true,123));
+        patientRepository.save(new Patient(null,"Fatima",new Date(),false,10));
+    }
+}
---
+++ src/main/java/ma/emsi/patientapp/repository/PatientRepository.java
@@ -0,0 +1,17 @@
+package ma.emsi.patientapp.repository;
+
+import ma.emsi.patientapp.entities.Patient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface PatientRepository extends JpaRepository<Patient,Long> {
+
+    Page<Patient> findByNomContains(String keyword,Pageable pageable);
+    @Query("select p from Patient p where p.nom like :x")
+    Page<Patient> chercher(@Param("x") String keyword, Pageable pageable);
+}
